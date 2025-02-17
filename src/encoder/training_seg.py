df1e0fd5f9a5cd1d5108da85c7e3cec7d7e4a66b
esrh <esrh@esrh.me>
2025-02-17 21:32:38 +0900

    training fixes & tsubame script

--- src/encoder/training_seg.py
+++ src/encoder/training_seg.py
@@ -1,6 +1,7 @@
 # (setq python-shell-interpreter "/home/esrh/csi_to_image/activate_docker.sh")
 # (setq python-shell-intepreter-args "-p")
 import sys
+import gc
 from mlp import MLP
 from data_utils import load_data
 from viz import test_model
@@ -17,11 +18,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", required=True)
 parser.add_argument("-s", "--save", action="store_true")
+parser.add_argument("-e", "--max-epochs", default=1, type=int)
+parser.add_argument("-b", "--batch-size", default=16, type=int)
 args = parser.parse_args()
 
 data_path = Path(args.path)
 dataset = load_data(data_path, aux_data=["seg_map"])
-data = DataLoader(dataset, num_workers=15, batch_size=32)
+data = DataLoader(dataset, num_workers=15, batch_size=args.batch_size)
 print("Loaded data")
 
 # ***
@@ -33,19 +36,22 @@ class CSIAutoencoder(L.LightningModule):
         # output: 4*60*80 = 19200
         super().__init__()
         self.model = model
-        self.feature_extractor = (
-            transformers.SegformerImageProcessor.from_pretrained(
-                "nvidia/segformer-b0-finetuned-ade-512-512"
-            )
-        )
+        # self.feature_extractor = (
+        #     transformers.AutoImageProcessor.from_pretrained(
+        #         "nvidia/segformer-b0-finetuned-ade-512-512",
+        #         use_fast=False  # Segformer processor has no fast version
+        #     )
+        # )
         self.segformer = (
             transformers.SegformerForSemanticSegmentation.from_pretrained(
                 "nvidia/segformer-b0-finetuned-ade-512-512"
             )
         )
         self.vae = diffusers.AutoencoderKL().from_pretrained(
-            data_path / "sd/sd-v1-5", subfolder="vae"
+            data_path.parents[1] / "sd/sd-v1-5", subfolder="vae",
         )
+        self.vae.enable_tiling()
+
         for param in self.segformer.parameters():
             param.requires_grad = False
         for param in self.vae.parameters():
@@ -56,6 +62,7 @@ class CSIAutoencoder(L.LightningModule):
 
     def training_step(self, batch, batch_idx):
         inputs, targets, segmaps = batch
+        # print(inputs.shape, targets.shape, segmaps.shape)
         outputs = self.model(inputs)
         if batch_idx % 2 == 0:
             loss = torch.nn.functional.mse_loss(outputs, targets)
@@ -65,15 +72,18 @@ class CSIAutoencoder(L.LightningModule):
             decoded = ((self.vae.decode(outputs).sample + 1) / 2).clamp(0, 1)
             logits = self.segformer(decoded).logits
             loss = torch.nn.functional.cross_entropy(logits, segmaps)
-            self.log("tr_seg_loss", loss)
+            del decoded
+            del logits
+            gc.collect()
+            torch.cuda.empty_cache()
             return loss
 
-    def validation_step(self, batch, batch_idx):
-        inputs, targets = batch
-        outputs = self.model(inputs)
-        loss = torch.nn.functional.mse_loss(outputs, targets)
-        self.log("val_loss", loss)
-        return loss
+    # def validation_step(self, batch, batch_idx):
+    #     inputs, targets = batch
+    #     outputs = self.model(inputs)
+    #     loss = torch.nn.functional.mse_loss(outputs, targets)
+    #     self.log("val_loss", loss)
+    #     return loss
 
     def configure_optimizers(self):
         return torch.optim.Adam(model.parameters(), lr=5e-4)
@@ -85,9 +95,10 @@ class CSIAutoencoder(L.LightningModule):
 # ***
 model = CSIAutoencoder(MLP([1992, 1000, 500, 250, 500, 1000, 2000, 16384]))
 trainer = L.Trainer(
-    max_epochs=250,
+    max_epochs=args.max_epochs,
     logger=L.pytorch.loggers.CSVLogger(save_dir=data_path / "logs"),
     strategy="ddp_find_unused_parameters_true",
+    precision=16
 )
 trainer.fit(model, data)
 
--- tsubame.sh
+++ tsubame.sh
@@ -4,18 +4,19 @@
 #$ -cwd
 
 # 資源タイプを指定（必須）
-#$ -l cpu_80=1
+#$ -l gpu_1=1
 # 実行時間を指定（必須；お試し実行機能は利用最長時間10分）
 #$ -l h_rt=23:59:59
 # ジョブ名を指定（任意）
-#$ -N opensvdd-ton-ids
+#$ -N tsubame_training_seg
 
 module load cuda
 module load intel
 
 nvidia-smi
-python3 -m pip install --user uv
-uv pip install torch torchvision numpy diffusers transformers accelerate lightning more-itertools
+python3 -m pip install --user torch torchvision numpy diffusers transformers accelerate lightning more-itertools
 
-python src/encoder/training_seg.py -p /gs/fs/tga-nlab/eshan/datasets/walking
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python3 src/encoder/training_seg.py -p /gs/fs/tga-nlab/eshan/csi_image_data/datasets/walking -b 16 -e 200 -s
+
+
 wait
