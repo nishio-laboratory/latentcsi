67d5c59a67bd7007ceec12d05ef207f1a3158c2e
esrh <esrh@esrh.me>
2025-03-03 00:14:00 +0900

    make docker script use cwd

--- activate_docker.sh
+++ activate_docker.sh
@@ -8,10 +8,10 @@ fi
 while getopts 'bpr' arg; do
     case $arg in
         p)
-            docker run --ipc=host -v /mnt/nas/esrh/csi_image_data:/data -v /home/esrh/csi_to_image/:/code --gpus="device=all" -it csi_image ipython -i --simple-prompt --InteractiveShell.display_page=True
+            docker run --ipc=host -v /mnt/nas/esrh/csi_image_data:/data -v .:/code --gpus="device=all" -it csi_image ipython -i --simple-prompt --InteractiveShell.display_page=True
             ;;
         b)
-            docker run --ipc=host -v /mnt/nas/esrh/csi_image_data:/data -v /home/esrh/csi_to_image/:/code --gpus="device=all" -it csi_image bash
+            docker run --ipc=host -v /mnt/nas/esrh/csi_image_data:/data -v .:/code --gpus="device=all" -it csi_image bash
             ;;
         r)
             docker ps | grep "csi_image" | cut -d " " -f 1 | xargs docker rm -f
